1d691ae35c3b675df05a851ff284b9d37dd75969
Ashi jain <ashijain1403@gmail.com>
2024-07-21 11:36:28 +0530

    Sort the array and iterate through each element, using two pointers to find all unique triplets that sum to zero. For each element, adjust the pointers and add the valid triplets to the result list, avoiding duplicates.

---
+++ threesum.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class threesum {
+
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum > 0) {
+                    r--;
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    l++;
+                    r--;
+                    while (l < r && nums[l] == nums[l - 1]) {
+                        l++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1,-4};
+        System.out.println(threeSum(nums));
+    }    
+}
+
+// time complexity -O(n^2)
+// space complexity -O(n)
