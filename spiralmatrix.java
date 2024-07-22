a02b0bb57cb3c69c593f4afae603b9a77e4b133c
Ashi jain <ashijain1403@gmail.com>
2024-07-22 23:19:12 +0530

    Traverse the matrix in a spiral order by iterating through the top row, right column, bottom row, and left column, adjusting the boundaries after each traversal. Add the elements to the result list until all boundaries meet.

---
+++ spiralmatrix.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class spiralmatrix {
+     public  static  List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if (matrix == null || matrix.length == 0) {
+            return result;
+        }
+        
+        int rows = matrix.length, cols = matrix[0].length;
+        int left = 0, right = cols-1, top = 0, bottom = rows-1;
+        
+        while (left <= right && top <= bottom) {
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+            
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+            
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        
+        return result;
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},{4,5,6,},{7,8,9}};
+        List<Integer> result = spiralOrder(matrix);
+        System.out.println(result);
+    }
+    
+
+    
+}
+// time complexity -0(m*n)
+// space complexity -0(n)
+
+
