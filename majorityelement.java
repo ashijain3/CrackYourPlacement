aefdfb14f635114f5550070b448f0a18a0ea4008
Ashi jain <ashijain1403@gmail.com>
2024-07-22 23:27:42 +0530

    Count the frequency of each element in the array using a hash map. Return the element with a count greater than half the size of the array.

---
+++ majorityelement.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class majorityelement {
+
+    public static int majorityElement(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        
+        n = n / 2;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > n) {
+                return entry.getKey();
+            }
+        }
+        
+        return 0;
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,2,1,1,1,2,2};
+        System.out.println(majorityElement(nums));
+    }
+    
+}
+//time complexity-O(n)
+//space complexity-O(n)
