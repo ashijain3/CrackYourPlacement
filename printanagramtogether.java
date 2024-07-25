8e76ff54510f8bb3442c813be210b3ddbec4006f
Ashi jain <ashijain1403@gmail.com>
2024-07-25 21:00:46 +0530

    Sort characters of each word, group words with the same sorted characters. Return all grouped anagrams as a list of lists.

---
+++ printanagramtogether.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class printanagramtogether {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String word : strs) {
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = new String(chars);
+            
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+            
+            map.get(sortedWord).add(word);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat","bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+    
+}
+
+//time complexity-O(n)
+//space complexity-O(n)
+
