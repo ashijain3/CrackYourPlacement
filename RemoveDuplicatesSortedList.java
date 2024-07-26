2e1aa3988ea1379b9b054c86ffcf3fc1926f77d4
Ashi jain <ashijain1403@gmail.com>
2024-07-26 23:16:20 +0530

    Iterate through the list, checking if the current node's value is equal to the next node's value. If they are equal, skip the next node; otherwise, move to the next node.

---
+++ RemoveDuplicatesSortedList.java
@@ -0,0 +1,55 @@
+public class RemoveDuplicatesSortedList {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode current = head;
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+        head.next.next.next.next.next = new ListNode(4);
+        head.next.next.next.next.next.next = new ListNode(4);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        head = deleteDuplicates(head);
+
+        System.out.println("List after removing duplicates:");
+        printList(head);
+    }
+}
+// time complexity -O(n)
+// space complexity -O(1)
