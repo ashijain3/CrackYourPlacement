509c5153558e9d0e7b37b1e5e1edcb43b3cada39
Ashi jain <ashijain1403@gmail.com>
2024-07-28 00:05:05 +0530

    If list1 or list2 is empty, return the non-empty list. Otherwise, compare the heads of list1 and list2, recursively merge the next nodes, and return the node with the smaller value.

---
+++ MergeTwoLists.java
@@ -0,0 +1,49 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+public class MergeTwoLists {
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                list1.next = mergeTwoLists(list1.next, list2);
+                return list1;
+            } else {
+                list2.next = mergeTwoLists(list1, list2.next);
+                return list2;
+            }
+        }
+        if (list1 == null)
+            return list2;
+        return list1;
+    }
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        ListNode mergedList = mergeTwoLists(list1, list2);
+
+        System.out.print("Merged List: ");
+        printList(mergedList);
+    }
+}
+
+//time complexity -O(n)
+//space complexity -O(1)
